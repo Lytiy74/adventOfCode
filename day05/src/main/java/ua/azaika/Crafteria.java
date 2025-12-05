c8cfdf2c2d39bea4187c3c1160271f88e36b152a
Andrii Zaika <lytiy7404@gmail.com>
2025-12-05 20:52:24 +0100

    day05 part 2 impl

--- day05/src/main/java/ua/azaika/Crafteria.java
+++ day05/src/main/java/ua/azaika/Crafteria.java
@@ -1,5 +1,6 @@
 package ua.azaika;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,28 +21,71 @@ public class Crafteria {
 
     public static void main(String[] args) {
         Crafteria crafteria = new Crafteria();
-        int solve = crafteria.solve(INPUT);
+        int solve = crafteria.solvePart1(INPUT);
         System.out.println(solve);
+        System.out.println(crafteria.solvePart2(INPUT));
     }
 
-    public int solve(String input) {
-        List<String> freshRanges = Arrays.stream(input.substring(0,input.indexOf("\n\n")).split("\n")).toList();
-        List<Long> ids = Arrays.stream(input.substring(input.indexOf("\n\n")+2).split("\n")).map(Long::parseLong).toList();
+    public int solvePart1(String input) {
+        List<String> freshRanges = Arrays.stream(input.substring(0, input.indexOf("\n\n")).split("\n")).toList();
+        List<Long> ids = Arrays.stream(input.substring(input.indexOf("\n\n") + 2).split("\n")).map(Long::parseLong).toList();
         int freshCount = 0;
 
         for (long id : ids) {
-            if(isFresh(id,freshRanges)) freshCount++;
+            if (isFreshPart1(id, freshRanges)) freshCount++;
         }
 
-          return freshCount;
+        return freshCount;
     }
 
-    private boolean isFresh(long id, List<String> freshRanges) {
+    private boolean isFreshPart1(long id, List<String> freshRanges) {
         for (String range : freshRanges) {
             long lowerBound = Long.parseLong(range.split("-")[0]);
             long upperBound = Long.parseLong(range.split("-")[1]);
-            if (id >= lowerBound && id <= upperBound) return  true;
+            if (id >= lowerBound && id <= upperBound) return true;
         }
-        return  false;
+        return false;
+    }
+
+    public long solvePart2(String input) {
+        List<Range> freshRanges = Arrays.stream(input.substring(0, input.indexOf("\n\n")).split("\n"))
+                .map(range -> new Range(Long.parseLong(range.split("-")[0]), Long.parseLong(range.split("-")[1])))
+                .sorted()
+                .toList();
+
+        List<Range> mergedRanges = mergeRanges(freshRanges);
+
+        long totalFreshIds = 0;
+        for (Range range : mergedRanges) {
+            totalFreshIds += (range.upperBound - range.lowerBound + 1);
+        }
+
+        return totalFreshIds;
+    }
+
+    private List<Range> mergeRanges(List<Range> freshRanges) {
+        List<Range> merged = new ArrayList<>();
+        Range currentMerged = freshRanges.getFirst();
+
+        for (int i = 1; i < freshRanges.size(); i++) {
+            Range next = freshRanges.get(i);
+
+            if (currentMerged.upperBound >= next.lowerBound) {
+                currentMerged = new Range(currentMerged.lowerBound, Math.max(currentMerged.upperBound, next.upperBound));
+            } else {
+                merged.add(currentMerged);
+                currentMerged = next;
+            }
+        }
+
+        merged.add(currentMerged);
+        return merged;
+    }
+
+    record Range(long lowerBound, long upperBound) implements Comparable<Range> {
+        @Override
+        public int compareTo(Range o) {
+            return Long.compare(lowerBound, o.lowerBound);
+        }
     }
 }
